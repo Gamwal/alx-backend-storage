9361b65ca02cf9809072d7d0bf3c5cda212b9407
Gamaliel Adun <adungamaliel@gmail.com>
2023-10-18 11:55:32 +0100

    added 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+function that changes all topics of
+a school document based on the name
+
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    function to updates documents in a collection
+
+    Args:
+        mongo_collection (pymongo collection object): db to query
+        name (dict): dictionary of documents to insert in ollection
+
+    Returns:
+        result (pymongo collection object): updated mongodb collection
+    """
+
+    result = mongo_collection.update(
+        {"name": name},
+        {"$set": {"topics": topics}}
+        )
+    return result
